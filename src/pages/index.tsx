5afd9605fcad64218aaa8405461eb018760d0eaa
Shlok D. Mange <shlok_mange@protonmail.com>
2023-02-05 21:47:40 +0530

    navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { NextPage } from 'next'
-import { Box, HStack, Heading, Stack, Text, Button } from '@chakra-ui/react'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Box, HStack, Heading, Stack, Text, Button, Link } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
-	const logo = `fil.swap`
 	
+	const logo = `fil.swap`
+	const [connectedAccount, setConnectedAccount] = useState('')
+
 	return(
-		<Box h={'100vh'} bgColor={'gray.900'} w={'full'}>
-			<HStack px={[4,8,12]} py={[4,8]} align={'center'} justify={'space-between'}>
-				<Heading color={'gray.100'} fontFamily={'mono'}>{logo}</Heading>
-				<ConnectButton />
+		<Box h={'100vh'} bgColor={'black'} w={'full'} fontFamily={'mono'}>
+			<HStack px={[4,8,12]} py={[4,8]} align={'baseline'} justify={'space-between'}>
+				<Heading color={'green.500'} fontFamily={'mono'}>{logo}</Heading>
+				<HStack spacing={8} color={'green.200'}>
+					<Link>Swap</Link>
+					<Link>Liquidity</Link>
+					{
+						connectedAccount ? 
+						<Text>{connectedAccount}</Text> :
+						<Button bgColor={'green.600'} color={'green.50'}>Connect Wallet</Button>
+					}
+				</HStack>
 			</HStack>
 		</Box>
 	)
